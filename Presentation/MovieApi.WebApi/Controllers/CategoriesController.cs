aededd9266edab2a0d04c1b3d498431dcdbc5557
Emirhan Yıldız <emirhanyy53@gmail.com>
2026-01-28 20:51:15 +0300

    feat: add GetMovie and GetCategory query handlers

---
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebApi.Controllers;
+
+
+[ApiController] 
+[Route("api/[controller]")] 
+public class MoviesController : ControllerBase 
+{
+    // private readonly GetQuery
+}
