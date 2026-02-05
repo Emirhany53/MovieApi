08f96610c35065f0adfa01b32fffdeda968d4bdf
Emirhan Yıldız <emirhanyy53@gmail.com>
2026-02-05 19:44:23 +0300

    Add UpdateCastCommand handler

--- Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/UpdateCastCommandHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/UpdateCastCommandHandler.cs
@@ -1,6 +1,30 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Persistence.Context;
+using NuGet.Protocol.Plugins;
+
 namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers;
 
-public class UpdateCastCommandHandler
+public class UpdateCastCommandHandler:IRequestHandler<UpdateCastComand>
 {
-    
+    private readonly MovieContext _context;
+
+    public UpdateCastCommandHandler(MovieContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(UpdateCastComand request, CancellationToken cancellationToken)
+    {
+        var values = await _context.Casts.FindAsync(request.CastId);
+        values.Surname = request.Surname;
+        values.Overview = request.Overview;
+        values.Biography= request.Biography;
+        values.ImageUrl = request.ImageUrl;
+        values.Name = request.Name;
+        values.Title= request.Title;
+        
+        await _context.SaveChangesAsync();
+    }
 }
