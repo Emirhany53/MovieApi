ba001858941cc35d74ac47c083bf7cc50debe29f
Emirhan Yıldız <emirhanyy53@gmail.com>
2026-02-21 13:28:09 +0300

    feat: implement GetTags query

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagQuery.cs
@@ -0,0 +1,16 @@
+using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.TagResults;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries;
+
+public class GetTagQuery:IRequest<List<GetTagQueryResult>>
+{
+    public GetTagQuery(int tagId)
+    {
+        TagId = tagId;
+    }
+
+    public int TagId { get; set; }
+    
+    
+}
