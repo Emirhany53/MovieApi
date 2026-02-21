a53f63d8b34fdd9a7e7e11faebdc2f6ba2e340f7
Emirhan Yıldız <emirhanyy53@gmail.com>
2026-02-21 13:31:49 +0300

    refactor: remove required modifier from Tag entity

--- Core/MovieApi.Domain/Entities/Tag.cs
+++ Core/MovieApi.Domain/Entities/Tag.cs
@@ -3,6 +3,6 @@ namespace MovieApi.Domain.Entities;
 public class Tag
 {
     public int TagId { get; set; }
-    public required string Title { get; set; }
+    public string Title { get; set; }
     
 }
